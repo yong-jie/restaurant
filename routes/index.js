188d83892fdc49d6426726634b610fc4ccf71f7a
yong-jie <viorama@gmail.com>
2019-02-28 23:17:41 +0800

    Implement login

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,52 @@ router.get("/", function(req, res, next) {
     return res.render("notloggedin");
   }
   // User is authenticated.
-  res.render("index", { title: "Express" });
+  res.render("index", { username: req.session.auth.username, type: req.session.auth.type });
 });
 
 router.get("/signup", (req, res, next) => {
   return res.render("signup");
 })
+
+router.get("/login", (req, res, next) => {
+  return res.render("login");
+})
+
+router.get("/logout", (req, res, next) => {
+  req.session.auth = false;
+  return res.redirect("/");
+})
+
+router.post("/login", async (req, res, next) => {
+  if (!(req.body && req.body.username && req.body.password)) {
+    return res.send("Missing fields");
+  }
+  
+  // First check users.
+  {
+    let { rows } = await pool.query(`SELECT * FROM users WHERE username = '${req.body.username}' AND password = '${req.body.password}'`);
+    if (rows.length === 0) return res.send("Incorrect credentials");
+  }
+
+  req.session.auth = { username: req.body.username};
+
+  // Now check what authorization this user is.
+  {
+    let { rows } = await pool.query(`SELECT * FROM diners WHERE username = '${req.body.username}'`);
+    if (rows.length !== 0) req.session.auth.type = 'Diners';
+  }
+  {
+    let { rows } = await pool.query(`SELECT * FROM admins WHERE username = '${req.body.username}'`);
+    if (rows.length !== 0) req.session.auth.type = 'Admins';
+  }
+  {
+    let { rows } = await pool.query(`SELECT * FROM owners WHERE username = '${req.body.username}'`);
+    if (rows.length !== 0) req.session.auth.type = 'Owners';
+  }
+
+  if (!req.session.auth.type) return res.send("You do not have an authorization level. This should not be happening.");
+  return res.redirect("/");
+});
 
 router.post("/signup", async (req, res, next) => {
   if (!(req.body && req.body.username && req.body.password && req.body.type)) {
@@ -49,5 +89,6 @@ router.post("/signup", async (req, res, next) => {
   }
   return res.redirect("/");
 })
+
 
 module.exports = router;
