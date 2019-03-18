3e981fc0e06caf5a46bc10f6ce17fec1de907eab
shouteck <shouteckie@gmail.com>
2019-03-18 11:16:48 +0800

    - Added 3 new relations Restaurants, Areas, Located. (#1)
    - Added Routing for /restaurants (js,ejs).

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const { Pool } = require("pg");
+const config = require("../config.js");
+const pool = new Pool(config.pgConfig);
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var sql_query = 'SELECT * FROM restaurants';
+	pool.query(sql_query, (err, data) => {
+		res.render('restaurants', { title: 'Participating Restaurants', data: data.rows });
+	});
 });
 
 module.exports = router;
