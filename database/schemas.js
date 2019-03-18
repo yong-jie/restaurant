fd976dbacd280ab83392283d1746f777785ff687
shouteck <shouteckie@gmail.com>
2019-03-18 10:59:30 +0800

    - Added 3 new relations Restaurants, Areas, Located. - Added Routing for /restaurants (js,ejs).

--- database/schemas.js
+++ database/schemas.js
@@ -79,6 +79,66 @@ const buildSchemas = async () => {
       },
     );
   });
+
+  // Delete Restaurant
+  await new Promise((resolve, reject) => {
+    pool.query("drop table Restaurants", (err, data) => {
+      if (err) return resolve(console.log("Restaurants table does not exist."));
+      return resolve(console.log("Deleted Restaurants table"));
+    });
+  });
+
+  // Create Restaurant
+  await new Promise((resolve, reject) => {
+    pool.query(
+      "create table Restaurants(rname VARCHAR(40) primary key)",
+      (err, data) => {
+        if (err) return resolve(console.log("Error creating Restaurants table"));
+        return resolve(console.log("Created Restaurants table"));
+      },
+    );
+  });  
+
+  // Delete Area
+  await new Promise((resolve, reject) => {
+    pool.query("drop table Areas", (err, data) => {
+      if (err) return resolve(console.log("Areas table does not exist."));
+      return resolve(console.log("Deleted Areas table"));
+    });
+  });
+
+  // Create Area
+  await new Promise((resolve, reject) => {
+    pool.query(
+      "create table Areas(aname VARCHAR(40) primary key)",
+      (err, data) => {
+        if (err) return resolve(console.log("Error creating Areas table"));
+        return resolve(console.log("Created Areas table"));
+      },
+    );
+  });  
+
+  // Delete Located
+  await new Promise((resolve, reject) => {
+    pool.query("drop table Located", (err, data) => {
+      if (err) return resolve(console.log("Located table does not exist."));
+      return resolve(console.log("Deleted Located table"));
+    });
+  });
+
+  // Create Located
+  await new Promise((resolve, reject) => {
+    pool.query(
+      "create table Located(rname VARCHAR(40) references Restaurants(rname) on delete CASCADE"
+      + ",aname VARCHAR(40) references Areas(aname) on delete CASCADE"
+      + ",address VARCHAR(40)"
+      + ",primary key (rname, aname))",
+      (err, data) => {
+        if (err) return resolve(console.log("Error creating Located table"));
+        return resolve(console.log("Created Located table"));
+      },
+    );
+  });  
 };
 
 buildSchemas();
