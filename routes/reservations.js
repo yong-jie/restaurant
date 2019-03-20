c0fb59a60c67c335e805001d2783cc9568f84175
sijieg <35765034+sijieg@users.noreply.github.com>
2019-03-20 19:20:08 +0800

    added reservation table(/reservation) and booking system(/insert) (#2)
    * added reservation table(/reservation) and booking system(/insert)
    
    * Added check regex fpr booking system

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const { Pool } = require("pg");
+const config = require("../config.js");
+const pool = new Pool(config.pgConfig);
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var sql_query = 'SELECT * FROM reservations';
+	pool.query(sql_query, (err, data) => {
+		res.render('reservations', { title: 'List of Reservations', data: data.rows });
+	});
 });
 
 module.exports = router;
