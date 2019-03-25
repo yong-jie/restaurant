c716e64ee79e3e431e6d2038ef3d39adfc899c5a
shouteck <shouteckie@gmail.com>
2019-03-25 20:54:22 +0800

    - Updated /restaurants (#3)
    - Added Navbar and Searchbar in /restaurants
    - Added Rates relation

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,10 +5,37 @@ const { Pool } = require("pg");
 const config = require("../config.js");
 const pool = new Pool(config.pgConfig);
 
+router.get('/search', function(req, res, next) {
+	var total_restaurants = 0;
+	var restaurant = "%" + req.query.rname.toLowerCase() + "%";
+	var sql_query_display = 'SELECT L.rname as rname, L.aname as aname, L.address as address, S.cname as cname,'
+	+ ' COALESCE((SELECT AVG(score) FROM rates where rname = L.rname), 0) AS score'
+	+ ' FROM located L natural join serves S'
+	+ ' WHERE lower(L.rname) LIKE $1'
+	+ ' ORDER BY score DESC';
+	var sql_query_count = 'SELECT count(*)'
+	+ ' FROM located L natural join serves S';
+	pool.query(sql_query_count, (err, data) => {
+		total_restaurants = data.rows;
+		pool.query(sql_query_display, [restaurant], (err, data) => {
+			res.render('restaurants', { title: 'Participating Outlets', data: data.rows, total: total_restaurants });
+		})
+	});
+});
+
 router.get('/', function(req, res, next) {
-  var sql_query = 'SELECT * FROM sells natural join located natural join serves';
-	pool.query(sql_query, (err, data) => {
-		res.render('restaurants', { title: 'Participating Restaurants', data: data.rows });
+	var total_restaurants = 0;
+	var sql_query_display = 'SELECT L.rname as rname, L.aname as aname, L.address as address, S.cname as cname,'
+	+ ' COALESCE((SELECT AVG(score) FROM rates where rname = L.rname), 0) AS score'
+	+ ' FROM located L natural join serves S'
+	+ ' ORDER BY score DESC';
+	var sql_query_count = 'SELECT count(*)'
+	+ ' FROM located L natural join serves S';
+	pool.query(sql_query_count, (err, data) => {
+		total_restaurants = data.rows;
+		pool.query(sql_query_display, (err, data) => {
+			res.render('restaurants', { title: 'Participating Outlets', data: data.rows, total: total_restaurants });
+		});
 	});
 });
 
