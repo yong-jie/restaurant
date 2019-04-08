ff514ec08fb953dbb3c64049e2732ebf148dd330
sijieg <dindis123@hotmail.com>
2019-04-09 06:13:36 +0800

    hi

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var menuRouter = require("./routes/menu");
 var dashboardRouter = require("./routes/dashboard");
 var likesRouter = require("./routes/likes");
 var owners_menuRouter = require("./routes/owners_menu");
+var diners_reservationsRouter = require("./routes/diners_reservations");
 
 var pgPool = new pg.Pool(config.pgConfig);
 var app = express();
@@ -64,6 +65,7 @@ app.use("/menu", menuRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/likes", likesRouter);
 app.use("/owners_menu", owners_menuRouter);
+app.use("/diners_reservations", diners_reservationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/diners_reservations.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+const { Pool } = require("pg");
+const config = require("../config.js");
+const pool = new Pool(config.pgConfig);
+
+router.get('/', function(req, res, next) {
+
+    var diner_username = 'Sijie';
+
+    var diners_reservation = 'SELECT reid,rname,aname,confirmed,numPax,dateTime as dt '
+    + ' FROM reserves'
+    + ' WHERE username = $1'
+    + ' ORDER BY reid ASC;'
+
+    pool.query(diners_reservation, [diner_username], (err, data) => {
+        res.render('diners_reservations', {title: 'Reservations', data: data.rows});
+    })
+})
+
+router.post('/', function(req, res, next) {
+
+    var diner_username = 'Sijie';
+
+    var sql_query1 = 'UPDATE RESERVES '
+    + 'SET CONFIRMED = ';
+    var sql_query3 = ' WHERE username = $1 AND';
+    var sql_query4 = ' REID = $2;';
+
+    var reid = req.body.reid;
+	var confirmed = req.body.confirmed;
+	
+	var update_query = sql_query1 + confirmed + sql_query3 + sql_query4;
+	pool.query(update_query, [diner_username, reid], (err, data) => {
+        console.error(update_query);
+		res.redirect('/diners_reservations')
+	});
+});
+
+module.exports = router;
